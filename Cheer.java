1865f08a052c7161d295d3c5acf4ad1b35cb0f47
Kornél Konyári <konyarikornel@gmail.com>
2023-12-04 17:18:13 +0100

    d6

--- Cheer.java
+++ Cheer.java
@@ -1,5 +1,5 @@
 public class Cheer {
-    public void cheering() {
-        System.out.println("java");
+    public void cheering(String param) {
+        System.out.println("Cheer for the " + param);
     }
 }
