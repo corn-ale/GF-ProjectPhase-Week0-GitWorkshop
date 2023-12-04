342e7affe0427fefcc08151ac3ee364876c03b6a
Marek Macko <marek.macko24@gmail.com>
2023-12-04 17:36:16 +0100

    b6

--- Greet.java
+++ Greet.java
@@ -1,9 +1,9 @@
 public class Greet {
     public static void main(String[] args) {
-        greet("Robert Horvath");
+        greet("Robert", "Horvath");
     }
 
-    public static void greet(){
-        System.out.println("Greetings!");
+    public static void greet(String firstName, String lastName){
+        System.out.println("Greetings for " + firstName + " " + lastName);
     }
 }
